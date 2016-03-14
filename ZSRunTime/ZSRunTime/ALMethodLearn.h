3a0993a47e7b1310b519ae603fe7ced5724774d6
alan.zhang <alan.zhang@kakaocorp.com>
2016-03-14 10:29:13 +0800

    methodLearn

---
+++ ZSRunTime/ZSRunTime/ALMethodLearn.h
@@ -0,0 +1,13 @@
+//
+//  ALMethodLearn.h
+//  ZSRunTime
+//
+//  Created by kakao on 16/3/14.
+//  Copyright © 2016年 shuai zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ALMethodLearn : NSObject
+
+@end
