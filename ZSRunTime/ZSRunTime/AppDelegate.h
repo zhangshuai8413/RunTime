ab1373019c0c52be29dabad60b41d403eecf555f
alan.zhang <alan.zhang@kakaocorp.com>
2016-03-14 09:40:00 +0800

    初始化项目

---
+++ ZSRunTime/ZSRunTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZSRunTime
+//
+//  Created by kakao on 16/3/14.
+//  Copyright © 2016年 shuai zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZSRunTime/ZSRunTime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZSRunTime
+//
+//  Created by kakao on 16/3/14.
+//  Copyright © 2016年 shuai zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
